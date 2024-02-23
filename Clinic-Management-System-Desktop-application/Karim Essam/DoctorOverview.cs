d515e62a9162e7ff8bb1e08d3dc9cdb98ad3c8f7
KaEssam <Kaessam17@gmail.com>
2024-02-24 00:15:59 +0200

    Add project

---
+++ Clinic-Management-System-Desktop-application/Karim Essam/DoctorOverview.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clinic_Management_System_Desktop_application.Karim_Essam
+{
+    public partial class DoctorOverview : Form
+    {
+        public DoctorOverview()
+        {
+            InitializeComponent();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            lbTime.Text = DateTime.Now.ToString("HH:mm");
+            lbSecond.Text = DateTime.Now.ToString("ss");
+            lbDate.Text = DateTime.Now.ToString("MMMM dd yyyy");
+
+        }
+
+        private void DoctorOverview_Load(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+    }
+}
